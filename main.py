6d5dfbb1ade9f448c705682c7917d485f517c64d
sladk <SladkovskyN@yandex.ru>
2023-04-13 20:59:31 +0500

    д

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from wtforms.validators import DataRequired
+from data import db_session
 
 app = Flask(__name__, template_folder='template')
 app.config['SECRET_KEY'] = 'art_secret_key'
-
+db_session.global_init("db/blogs.db")
+app.run()
 
 @app.route('/chest')
 def chest():
@@ -32,6 +34,32 @@ def chest():
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+@app.route('/success')
+def success():
+    return """"<!doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link rel="stylesheet"
+                  href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                  integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                  crossorigin="anonymous">
+            <title>Rat</title>
+        </head>
+        <body>
+                <div align="right">
+                    <a href="/login" class="btn btn-primary">профиль</a>
+                    <a type="submit" class="btn btn-primary">выйти</a>
+                </div>
+                <h1 align="center">RAT</h1>
+                <h3 style="margin-top: 100px;" align="center">Base</h3>
+                <div align="center">
+                    <img filename='data/Chest.jpg', width="100", height="111"/>
+                </div>
+        </body>
+    </html>"""
 
 
 @app.route('/login', methods=['GET', 'POST'])

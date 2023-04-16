a46dd0254771a11a005c928c635492b8ea57ff07
sladk <SladkovskyN@yandex.ru>
2023-04-16 15:54:58 +0500

    д

--- data/users.py
+++ data/users.py
@@ -1,14 +1,23 @@
 import sqlalchemy
 from .db_session import SqlAlchemyBase
-from werkzeug.security import generate_password_hash
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    submit = SubmitField('Войти')
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
+
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
 from flask import Flask, request, render_template, redirect
 from wtforms.validators import DataRequired
 from data import db_session
 from forms.user import RegisterForm
+from data.users import User
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 app = Flask(__name__, template_folder='template')
 app.config['SECRET_KEY'] = 'art_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 @app.route('/chest')
 def chest():
@@ -92,7 +107,6 @@ def reqister():
         user = User(
             name=form.name.data,
             email=form.email.data,
-            about=form.about.data
         )
         user.set_password(form.password.data)
         db_sess.add(user)
@@ -100,12 +114,10 @@ def reqister():
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
 
-class LoginForm(FlaskForm):
-    name = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
-
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 if __name__ == '__main__':
     db_session.global_init("db/blogs.db")
